cc13b1833e6c89645187e875d0011e2223bc1768
benjamin-lamon <152487148+benjamin-lamon@users.noreply.github.com>
2024-05-24 11:07:19 +0200

    Commit que je n'ai pas du tout oublié de faire
    vraiment pas.

--- Snake-EI3-2024/snek.c
+++ Snake-EI3-2024/snek.c
@@ -1,7 +1,8 @@
 /*
-author: bain a main, whoever that is...
+author: bain à main, whoever that is...
 */
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include "snakeAPI.h"
 
@@ -11,6 +12,20 @@ void main(){
     int sizeX;
     int sizeY;
     int nbWalls;
-    char gameType[150] = "TRAINING SUPER_PLAYER";
+    char gameType[150] = "TRAINING SUPER_PLAYER difficulty=1 timeout=5 seed=123 start=0";
     waitForSnakeGame(gameType, gameName, &sizeX, &sizeY, &nbWalls);
+    // waitForSnakeGame(gameType, gameName, &sizeX, &sizeY, &nbWalls);
+    int walls[nbWalls];
+    getSnakeArena(&walls);
+    int test = 1;
+    t_move move;
+    t_move moveAdv;
+    while (test == 1){
+        printArena();
+        sleep(1.5);
+        scanf("%d",&move);
+        sendMove(move);
+        getMove(&moveAdv);
+    }
+    printArena();
 };
